b53611195c0a5662535c9f1eda4c7a4c2b143ffd
shawon1fb <shawon0187@gmail.com>
2021-10-04 00:27:35 +0600

    admin, staff ,installer roal added 2

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,31 @@
-export class Task {}
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Task extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  details: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  public createdAt: Date;
+  /**
+   * DB last update time.
+   */
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  public updatedAt: Date;
+}
